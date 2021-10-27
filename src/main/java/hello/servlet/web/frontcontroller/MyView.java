b6482a23bb752c859a9f477c6b6a9665d5eaf1a8
Lucie Choi <luce.the.bright@gmail.com>
2021-10-27 21:23:01 +0900

    [section4] Model 추가 - v3

--- src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
     /**
@@ -21,5 +22,16 @@ public class MyView {
             throws ServletException, IOException {
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);// jsp 실행
+    }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        modelToRequestAttribute(model,request);
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request,response);
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+        model.forEach((key,value)->request.setAttribute(key,value));
     }
 }
